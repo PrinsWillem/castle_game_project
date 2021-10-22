4c558a3860a2878550944ba224d980dd0155f872
Willem Prins <willemprins@willems-mbp.home>
2021-10-22 16:18:30 +0100

    added data to database

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -5,19 +5,11 @@ const createRouter = function (collection) {
 
     const router = express.Router();
     
-    // INDEX get all bookings from MongoDB, and serve as json
+    // INDEX get all cards from MongoDB, and serve as json
 
 
-    // SHOW, get a booking from MongoDB by id, and serve as json
+    // SHOW, get a card from MongoDB by id, and serve as json
 
-  
-    // CREATE, Post a new booking, and persist to the database
-
-
-    // DESTROY, Delete a booking via its ID
-
-  
-    // UPDATE, update a booking that already exists via its ID
 
     return router;
 };
