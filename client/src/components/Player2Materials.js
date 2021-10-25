b9866d911a6d72b52ae6100003fd1b733af7472a
Willem Prins <willemprins@Willems-MacBook-Pro.local>
2021-10-25 21:13:12 +0100

    added Player2Materials and Player2Tools

---
+++ client/src/components/Player2Materials.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Player2Materials = ({player2Materials}) => {
+
+     const player2Cards = player2Materials.map((card) => {
+          return <li className="player-card" key={card._id}><img src={card.img} alt={card.name}/></li>
+      });  
+
+     return(
+          <p >{player2Cards}</p>
+     );
+
+};
+
+export default Player2Materials;
---
+++ client/src/components/Player2Tools.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Player2Tools = ({player2Tools}) => {
+
+     const player2Cards = player2Tools.map((card) => {
+          return <li className="player-card" key={card._id}><img src={card.img} alt={card.name}/></li>
+      });  
+
+     return(
+          <p >{player2Cards}</p>
+     );
+
+};
+
+export default Player2Tools;
