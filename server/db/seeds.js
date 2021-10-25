7d14a976b5f92aac4b472be519c84ef362edeb11
ByAnthony <anthony.byledbal@icloud.com>
2021-10-25 16:35:52 +0100

    can add tool card to played cards

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -279,121 +279,145 @@ db.cards.insertMany([
   },
   {
     "name": "Carpenter",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/carpenter.png"
 },
 {
     "name": "Carpenter",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/carpenter.png"
 },
 {
     "name": "Blacksmith",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/blacksmith.png"
 },
 {
     "name": "Blacksmith",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/blacksmith.png"
 },
 {
     "name": "Mortar_Mixer",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/mortar_mixer.png"
 },
 {
     "name": "Mortar_Mixer",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/mortar_mixer.png"
 },
 {
     "name": "Mason",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/mason.png"
 },
 {
     "name": "Mason",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/mason.png"
 },
 {
     "name": "Stone_Mason",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/stone_mason.png"
 },
 {
     "name": "Stone_Mason",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/stone_mason.png"
 },
 {
     "name": "Digger",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/digger.png"
 },
 {
     "name": "Digger",
+    "type": "tool",
     "value": 2,
     "img": "./images/tools/digger.png"
 },
 {
     "name": "13-Knot_Rope",
+    "type": "tool",
     "value": 4,
     "img": "./images/tools/13-knot_rope.png"
 },
 {
     "name": "13-Knot_Rope",
+    "type": "tool",
     "value": 4,
     "img": "./images/tools/13-knot_rope.png"
 },
 {
     "name": "13-Knot_Rope",
+    "type": "tool",
     "value": 4,
     "img": "./images/tools/13-knot_rope.png"
 },
 {
     "name": "13-Knot_Rope",
+    "type": "tool",
     "value": 4,
     "img": "./images/tools/13-knot_rope.png"
 },
 {
     "name": "13-Knot_Rope",
+    "type": "tool",
     "value": 4,
     "img": "./images/tools/13-knot_rope.png"
 },
 {
     "name": "13-Knot_Rope",
+    "type": "tool",
     "value": 4,
     "img": "./images/tools/13-knot_rope.png"
 },
 {
     "name": "Measuring_Rode",
+    "type": "tool",
     "value": 6,
     "img": "./images/tools/measuring_rode.png"
 },
 {
     "name": "Measuring_Rode",
+    "type": "tool",
     "value": 6,
     "img": "./images/tools/measuring_rode.png"
 },
 {
     "name": "Measuring_Rode",
+    "type": "tool",
     "value": 6,
     "img": "./images/tools/measuring_rode.png"
 },
 {
     "name": "Wheelbarrow",
+    "type": "tool",
     "value": 8,
     "img": "./images/tools/wheelbarrow.png"
 },
 {
     "name": "Wheelbarrow",
+    "type": "tool",
     "value": 8,
     "img": "./images/tools/wheelbarrow.png"
 },
 {
     "name": "Treadwheel_Crane",
+    "type": "tool",
     "value": 10,
     "img": "./images/tools/treadwheel_crane.png"
 }
